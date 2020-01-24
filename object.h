55f91c118774e9cb11cff4e6280ec91a3f29dd03
nithinsaripalli <nsaripalli@addgene.org>
2020-01-24 16:03:23 -0500

    extra code

--- object.h
+++ object.h
@@ -19,12 +19,10 @@ public:
 
     // Calculates the hash of this object, based on the helper <hash_me_>
     size_t hash() {
-        return reinterpret_cast<size_t>(this);
     }
 
     // default equality calculation of an object, should be overrided
     // by any subclasses that care (or overload hash_me_)
     virtual bool equals(Object *other) {
-        return this == other;
     }
 };
--- string.h
+++ string.h
@@ -20,53 +20,30 @@ public:
     size_t size_;
 
     String(char* c) {
-        size_ = strlen(c);
-        val = new char[size_ + 1];
-        strcpy(val, c);
     }
 
     String(const char* c) {
-        size_ = strlen(c);
-        val = new char[size_ + 1];
-        strcpy(val, c);
     }
 
     ~String() { //destructor
-        delete[] val;
     }
 
     // hash function based on the equivalence specified by <equals>
     size_t hash() {
-        size_t res = 0;
-        for (size_t i=0; i<size_; i++) {
-            res += val[i];
-        }
-        return res;
     }
 
     // compares strings based on direct character equivalence
     bool equals(Object *other) {
-        if (other == nullptr) return false;
-        String *s = dynamic_cast<String*>(other);
-        if (s == nullptr) return false;
-        return strcmp(val, s->val) == 0;
     }
 
     // compares the strings based on alphabetical order
     virtual int cmp(String *that) {
-        return strcmp(val, that->val);
     }
 
     // returns a string made up of the concatination of this string and s
     String* concat(String *s) {
-        char* res = new char[size_ + s->size_ + 1];
-        strcpy(res, val);
-        res = strcat(res, s->val);
-        String* strres = new String(res);
-        delete[] res;
-        return strres;
     }
 
     // getter for the size of this string
-    size_t size() { return size_; }
+    size_t size() {}
 };
